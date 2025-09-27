7ca2ee3e35f3662db4806643fb076ce34ac5652b
Juwoojae <juwqq1234@naver.com>
2025-09-27 22:35:06 +0900

    오류 코드와 메세지 처리 2

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -48,19 +48,19 @@ public class ValidationItemControllerV2 {
 
     /**
      * @param bindingResult : V1 에서 errors 를 담당한다
+     * BindingResult
+     * 스프링이 제공하는 검증 오류를 보관하는 객체이다. 검증 오류가 발생하면 여기에 보관하면 된다.
+     *
+     * BindingResult 가 없으면 -> 400 오류가 발생하면서 컨트롤러가 호출되지 않고, 오류 페이지로 이동한다
+     * BindingResult 가 있으면 -> @modelAttribute 의 객체에 타입 오류 등으로 바인딩이 실패하는 경우
+     *    오류 정보(FieldError) 를 BindingResult에 담아서 컨트롤러를 정상 호출한다
+     *
+     *
      */
 //    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         Item savedItem = itemRepository.save(item);
 
-        /**
-         * 오류가 발생한 객체 이름, 오류 필드, 사용자가 입력한 값(거절된 값) 타입 오류 같은 바인딩 실패인지, 검증 실패인지 구분하는 값
-         * 메세지 코드, 메세지에서 사용하는 인자, 기본 오류 메세지
-         * FieldError(String objectName, String field, @Nullable Object rejectedValue, boolean bindingFailure,
-         * 			            @Nullable String[] codes, @Nullable Object[] arguments, @Nullable String defaultMessage)
-         * 사용자의 입력 데이터가 컨트롤러에 @ModelAttribute 에 바인딩 되는 시점에 오류가 발생하면, 모델 객체에 사용자 입력 값을
-         * 유지하기 어렵다. 별도로 보관하기위해서 FieldError 가 파라메터로 받아서 유지한다
-         */
         if (!StringUtils.hasText(item.getItemName())) {
             bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
         }
@@ -90,9 +90,13 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
-
     /**
-     * @param bindingResult : V1 에서 errors 를 담당한다
+     * 오류가 발생한 객체 이름, 오류 필드, 사용자가 입력한 값(거절된 값) 타입 오류 같은 바인딩 실패인지, 검증 실패인지 구분하는 값
+     * 메세지 코드, 메세지에서 사용하는 인자, 기본 오류 메세지
+     * FieldError(String objectName, String field, @Nullable Object rejectedValue, boolean bindingFailure,
+     * 			            @Nullable String[] codes, @Nullable Object[] arguments, @Nullable String defaultMessage)
+     * 사용자의 입력 데이터가 컨트롤러에 @ModelAttribute 에 바인딩 되는 시점에 오류가 발생하면, 모델 객체에 사용자 입력 값을
+     * 유지하기 어렵다. 별도로 보관하기위해서 FieldError 가 파라메터로 받아서 유지한다
      */
     @PostMapping("/add")
     public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
@@ -127,7 +131,39 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        Item savedItem = itemRepository.save(item);
 
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName","required.default "},null,null));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item",  "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity",item.getQuantity(),false,new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+        }
+
+        //특정 필드가 아님 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"},new Object[]{10000,resultPrice} ,null));
+            }
+        }
+
+        //오류를 가지고 있다면
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+        //성공 로직
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
